4ff99b7c3992bea726a34eef30baadf09d7736f2
dmarchese76 <d.marchese76@journalism.cuny.edu>
2025-09-26 16:45:09 -0400

    Added map, headline and chatter

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,32 @@
 // Write your code!
-console.log("Hello bakers");
+alert('Hello, World!');
+// Import dependencies
+import * as L from 'leaflet';
+
+// Import data
+import homicides from '../_data/harvard_park_homicides.json';
+
+// Set the id of the div on the page where the map will go
+const divId = 'map';
+
+// Create the map
+const map = L.map(divId, {
+  scrollWheelZoom: false,
+});
+
+// Add a map layer
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  minZoom: 13,
+}).addTo(map);
+
+// Set the center and zoom
+map.setView([33.983265, -118.306799], 18);
+
+// Load the data
+homicides.forEach((obj) => {
+  L.circleMarker([obj.latitude, obj.longitude]) // As a pin ...
+    .addTo(map)
+    .bindTooltip(obj.first_name + ' ' + obj.last_name, { permanent: true }); // ... with a tooltip
+});
